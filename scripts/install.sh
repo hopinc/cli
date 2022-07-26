7645d6702ebe7758ed2540092b22543227af3e40
pxseu <57842793+pxseu@users.noreply.github.com>
2022-07-26 06:31:22 +0200

    feat: install script

---
+++ scripts/install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+set -eu
+
+#  If the version is not specified, use the latest version.
+VERSION=${HOP_VERSION:-"latest"}
+
+# If the install directory is not set, set it to a default value
+INSTALL_DIR=${HOP_INSTALL_DIR:-"/usr/local/bin"}
+
+PLATFORM=`uname -s`
+ARCH=`uname -m`
+
+if [[ $PLATFORM == CYGWIN* ]] || [[ $PLATFORM == MINGW* ]] || [[ $PLATFORM == MSYS* ]]; then
+    PLATFORM="Windows"
+fi
+
+if [[ $ARCH == armv8* ]] || [[ $ARCH == arm64* ]] || [[ $ARCH == aarch64* ]]; then
+    ARCH="aarch64"
+fi
+
+if [[ $ARCH == armv6* ]] || [[ $ARCH == armv7* ]]; then
+    ARCH="armv7"
+fi
+
+if [ -z ${INSTALL_PATH+x} ]; then
+    INSTALL_PATH="${INSTALL_DIR}/hop"
+fi
+
+DOWNLOAD_URL="https://github.com/hopinc/hop_cli/releases/${VERSION}/download/hop-${ARCH}-${PLATFORM}.$([ $PLATFORM = "Windows" ] && echo "zip" || echo "tar.gz"  )"
+
+echo "This script will automatically install hop (${VERSION}) for you."
+echo "Installation path: ${INSTALL_PATH}"
+
+if [ "x$(id -u)" == "x0" ]; then
+    echo "Warning: this script is currently running as root. This is dangerous. "
+    echo "         Instead run it as normal user. We will sudo as needed."
+fi
+
+if [ -f "$INSTALL_PATH" ]; then
+    echo "error: hop is already installed."
+    echo "  run \"hop update\" to update to latest version"
+    exit 1
+fi
+
+if ! hash curl 2> /dev/null; then
+    echo "error: you do not have 'curl' installed which is required for this script."
+    exit 1
+fi
+
+TEMP_FILE=`mktemp "${TMPDIR:-/tmp}/hop-tar.XXXXXXXXXX.tar.gz"`
+TEMP_HEADER_FILE=`mktemp "${TMPDIR:-/tmp}/hop-headers.XXXXXXXXXX"`
+
+cleanup() {
+    rm -f "$TEMP_FILE"
+    rm -f "$TEMP_HEADER_FILE"
+}
+
+trap cleanup EXIT
+HTTP_CODE=$(curl -SL --progress-bar "$DOWNLOAD_URL" -D "$TEMP_HEADER_FILE" --output "$TEMP_FILE" --write-out "%{http_code}")
+if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]]; then
+    echo "error: your platform and architecture (${ARCH}-${PLATFORM}) is unsupported."
+    exit 1
+fi
+
+EXTRACT_DIR=`mktemp -d "${TMPDIR:-/tmp}/hop-extract-dir.XXXXXXXXXX"`
+EXTRACTED_FILE=`mktemp "${TMPDIR:-/tmp}/hop.XXXXXXXXXX"`
+
+# untar or unzip the file
+if [ $PLATFORM = "Windows" ]; then
+    unzip -o "${TEMP_FILE}" -d "${EXTRACT_DIR}"
+    mv "${EXTRACT_DIR}/hop.exe" "${EXTRACTED_FILE}"
+else
+    tar -xzf "${TEMP_FILE}" -C "${EXTRACT_DIR}"
+    mv "${EXTRACT_DIR}/hop" "${EXTRACTED_FILE}"
+fi
+
+chmod 0755 "${EXTRACTED_FILE}"
+if ! mv "${TEMP_FILE}" "${INSTALL_PATH}" 2> /dev/null; then
+    echo "sudo is required to install hop to ${INSTALL_DIR}"
+    sudo -k mv "${EXTRACTED_FILE}" "${INSTALL_PATH}"
+fi
+
+echo "
+             \\\\
+        ,-~~~-\\\\_
+       (        .\    Hop CLI is now installed
+\\    / @\___(__--'    Start off by using: hop auth login
+ \\  /
+ hop!"
