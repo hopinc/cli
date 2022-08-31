d7c3e4f11a453fda9a27ef9afab700a25dcafde0
pxseu <57842793+pxseu@users.noreply.github.com>
2022-09-01 00:05:34 +0200

    feat: fix windows build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,7 @@
 # powershell -ExecutionPolicy Bypass -File .\scripts\build.ps1
 
-cargo build --release --target x86_64-pc-windows-msvc
+cargo build --release --target x86_64-pc-windows-msvc --package hop-cli
 
-cargo wix -I .\build\windows\main.wxs -v --nocapture --target x86_64-pc-windows-msvc --output target/wix/hop-x86_64-pc-windows-msvc.msi
+cargo wix -I .\build\windows\main.wxs -v --nocapture --target x86_64-pc-windows-msvc --output target/wix/hop-x86_64-pc-windows-msvc.msi --package hop-cli
 
 target/wix/hop-x86_64-pc-windows-msvc.msi
