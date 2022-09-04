2af5d3a83048e88740ae8ba053444e624645f084
pxseu <57842793+pxseu@users.noreply.github.com>
2022-09-05 01:20:28 +0200

    feat: delete sessions on logout

--- src/commands/auth/logout.rs
+++ src/commands/auth/logout.rs
@@ -1,6 +1,8 @@
-use anyhow::Result;
+use anyhow::{anyhow, ensure, Result};
 use clap::Parser;
+use serde_json::Value;
 
+use crate::state::http::HttpClient;
 use crate::state::State;
 use crate::store::context::Context;
 
@@ -11,17 +13,50 @@ pub struct Options {}
 pub async fn handle(_options: Options, mut state: State) -> Result<()> {
     let user_id = state.ctx.default_user;
 
-    assert!(user_id.is_some(), "You are not logged in.");
+    ensure!(user_id.is_some(), "You are not logged in.");
+
+    invalidate_token(
+        &state.http,
+        state
+            .auth
+            .authorized
+            .get(user_id.as_ref().unwrap())
+            .unwrap(),
+    )
+    .await?;
 
     // clear all state
     state.ctx = Context::default();
     state.ctx.save().await?;
 
     // remove the user from the store
-    state.auth.authorized.remove(&user_id.unwrap());
+    state.auth.authorized.remove(user_id.as_ref().unwrap());
     state.auth.save().await?;
 
     log::info!("You have been logged out");
+
+    Ok(())
+}
+
+async fn invalidate_token(http: &HttpClient, token: &str) -> Result<()> {
+    match token.split('_').next() {
+        Some("bearer") => {
+            http.request::<Value>("POST", "/auth/logout", None).await?;
+        }
+
+        Some("pat") => {
+            http.request::<Value>("DELETE", &format!("/users/@me/pats/{token}"), None)
+                .await?;
+        }
+
+        Some("ptk") => {
+            log::warn!("Project tokens are not invalidated on logout, please revoke them manually.");
+        }
+
+        _ => {
+            return Err(anyhow!("Unknown token type"));
+        }
+    }
 
     Ok(())
 }
