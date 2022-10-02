7814b4cd34795d2de7f516a3f854db03471376af
pxseu <57842793+pxseu@users.noreply.github.com>
2022-10-02 19:27:27 +0200

    feat: fix paths

--- src/store/utils.rs
+++ src/store/utils.rs
@@ -1,9 +1,11 @@
 use std::path::PathBuf;
 
 pub fn home_path(to_join: &str) -> PathBuf {
-    dirs::home_dir()
+    let path = dirs::home_dir()
         .expect("Could not find `home` directory")
-        .join(to_join)
-        .canonicalize()
-        .unwrap()
+        .join(to_join);
+
+    log::debug!("Home path + joined: {:?}", path);
+
+    path
 }
